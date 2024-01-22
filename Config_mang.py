741394c1b634c4d6ac016ee99c383afa5a65e985
Harshithuppar <upparh1609@gmail.com>
2024-01-22 17:23:40 +0530

    Made changes on Config_mang

--- Config_mang.py
+++ Config_mang.py
@@ -1,23 +1,6 @@
 import configparser
 import json
 
-if __name__ == "__main__":
-
-    Result = config_Parse()
-
-    if Result:
-        print("Configuration File Parser Results:")
-        for section, values in Result.items():
-            print(f"\n{section}:")
-            for key, value in values.items():
-                print(f"- {key}: {value}")
-
-    json_output = json.dumps(Result, indent=2)
-    with open("result_output.json", "w") as json_file:
-        json_file.write(json_output)
-
-    print("\nJSON data saved to 'result_output.json'")
-
 def config_Parse():
     try:
         config = configparser.ConfigParser
@@ -35,11 +18,21 @@ def config_Parse():
         print(f"Error: {e}")
 
 
-         
+  
+if __name__ == "__main__":
+    
+    File_path = "C:\Users\harsh\OneDrive\Documents\Devops\Assignments\New Text Document.ini"
+    Result = config_Parse(File_path)
 
+    if Result:
+        print("Configuration File Parser Results:")
+        for section, values in Result.items():
+            print(f"\n{section}:")
+            for key, value in values.items():
+                print(f"- {key}: {value}")
 
+    json_output = json.dumps(Result, indent=2)
+    with open("result_output.json", "w") as json_file:
+        json_file.write(json_output)
 
-      
-
-
-
+    print("\nJSON data saved to 'result_output.json'")
